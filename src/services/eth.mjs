2f54315a9b17c68a220b228bc1319759fa8b61ae
Tim Daubenschütz <tim@daubenschuetz.de>
2022-04-06 15:44:39 +0200

    Add blockNumber rpc call as job

--- src/services/eth.mjs
+++ src/services/eth.mjs
@@ -1,13 +1,14 @@
 //@format
-import { getBlockByNumber, getTransactionReceipt } from "eth-fun";
+import { blockNumber, getBlockByNumber, getTransactionReceipt } from "eth-fun";
 
 export async function translate(options, method, params) {
   if (method === "eth_getTransactionReceipt") {
     return await getTransactionReceipt(options, params[0]);
-  }
-  if (method === "eth_getBlockByNumber") {
+  } else if (method === "eth_getBlockByNumber") {
     // NOTE: `getBlockByNumber` expects the `blockNumber` input to be an
     // hexadecimal (`0x...`) value.
     return await getBlockByNumber(options, ...params);
+  } else if (method == "eth_blockNumber") {
+    return await blockNumber(options);
   }
 }
