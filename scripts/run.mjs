c9ed8674f68bab6f016e2ca1f23505f5bd9c8d53
Tim Daubenschütz <tim@daubenschuetz.de>
2022-04-06 15:43:30 +0200

    Improve strategy runner

--- scripts/run.mjs
+++ scripts/run.mjs
@@ -1,21 +1,25 @@
 // @format
+import "dotenv/config";
 import { Worker } from "worker_threads";
 import { resolve } from "path";
 
 import { toHex } from "eth-fun";
 
+import logger from "../src/services/extractor/logger.mjs";
 import { __dirname } from "../src/node_filler.mjs";
 import { run } from "../src/strategies/src/block_iterator/index.mjs";
 
 const extractorPath = resolve(__dirname, "./services/extractor/worker.mjs");
 
-const workerData = { concurrency: 10 };
+const workerData = { concurrency: 20 };
 const worker = new Worker(extractorPath, {
   workerData,
 });
-const options = {
-  worker,
-  startBlock: toHex(14531600),
-};
 
-run(options);
+const inputs = {
+  worker,
+  startBlock: toHex(14532654),
+};
+// TODO: We need to allow getting worker from elsewhere and not through
+// injection.
+run(inputs, logger);
