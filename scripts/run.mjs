320164bffd7635e94a650e5a91026f55b3be8b7c
Tim Daubenschütz <tim@daubenschuetz.de>
2022-04-06 12:02:05 +0200

    Implement simple extraction strategy
    - Fixes #17

---
+++ scripts/run.mjs
@@ -0,0 +1,21 @@
+// @format
+import { Worker } from "worker_threads";
+import { resolve } from "path";
+
+import { toHex } from "eth-fun";
+
+import { __dirname } from "../src/node_filler.mjs";
+import { run } from "../strategies/src/block_iterator/index.mjs";
+
+const extractorPath = resolve(__dirname, "./services/extractor/worker.mjs");
+
+const workerData = { concurrency: 10 };
+const worker = new Worker(extractorPath, {
+  workerData,
+});
+const options = {
+  worker,
+  startBlock: toHex(14531600),
+};
+
+run(options);
