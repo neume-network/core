c2e9db4cf68a923aed00590e67854056a2c97298
Tim Daubenschütz <tim@daubenschuetz.de>
2022-04-14 17:41:10 +0200

    Add crawling `tokenURI` to web3_subgraph strategy

--- src/services/extractor/eth.mjs
+++ src/services/extractor/eth.mjs
@@ -1,5 +1,10 @@
 //@format
-import { blockNumber, getBlockByNumber, getTransactionReceipt } from "eth-fun";
+import {
+  call,
+  blockNumber,
+  getBlockByNumber,
+  getTransactionReceipt,
+} from "eth-fun";
 
 import { NotImplementedError } from "../../errors.mjs";
 
@@ -10,8 +15,11 @@ export async function translate(options, method, params) {
     // NOTE: `getBlockByNumber` expects the `blockNumber` input to be an
     // hexadecimal (`0x...`) value.
     return await getBlockByNumber(options, ...params);
-  } else if (method == "eth_blockNumber") {
+  } else if (method === "eth_blockNumber") {
     return await blockNumber(options);
+  } else if (method === "eth_call") {
+    const { from, to, data } = params[0];
+    return await call(options, from, to, data, params[1]);
   } else {
     throw new NotImplementedError();
   }
