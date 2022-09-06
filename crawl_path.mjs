54ce47e17cc804d587060b443f57d40bd420dc0b
Tim Daubenschütz <tim@daubenschuetz.de>
2022-09-06 21:36:36 +0200

    Update crawl_path.mjs

--- crawl_path.mjs
+++ crawl_path.mjs
@@ -4,38 +4,65 @@ import { env } from "process";
 // crawlPath[i] and crawlPath[i+1] are executed in sequence
 // crawlPath[i][j] and crawlPath[i][j+1] are executed in parallel
 export default [
-  [{ name: "web3subgraph", extractor: {}, transformer: {} }],
   [
+    {
+      name: "catalog-call-tokenuri",
+      extractor: {
+        args: [resolve(env.DATA_DIR, "logs-to-subgraph-transformation")],
+      },
+      transformer: {},
+    },
+    {
+      name: "mintsongs-call-tokenuri",
+      extractor: {
+        args: [resolve(env.DATA_DIR, "logs-to-subgraph-transformation")],
+      },
+      transformer: {},
+    },
     {
       name: "soundxyz-call-tokenuri",
       extractor: {
-        args: [resolve(env.DATA_DIR, "web3subgraph-transformation")],
+        args: [resolve(env.DATA_DIR, "logs-to-subgraph-transformation")],
       },
       transformer: {},
     },
     {
       name: "zora-call-tokenuri",
       extractor: {
-        args: [resolve(env.DATA_DIR, "web3subgraph-transformation")],
+        args: [resolve(env.DATA_DIR, "logs-to-subgraph-transformation")],
       },
       transformer: {},
     },
     {
       name: "zora-call-tokenmetadatauri",
       extractor: {
-        args: [resolve(env.DATA_DIR, "web3subgraph-transformation")],
+        args: [resolve(env.DATA_DIR, "logs-to-subgraph-transformation")],
       },
       transformer: {},
     },
     {
       name: "soundxyz-metadata",
       extractor: {
-        args: [resolve(env.DATA_DIR, "web3subgraph-transformation")],
+        args: [resolve(env.DATA_DIR, "logs-to-subgraph-transformation")],
       },
       transformer: {},
     },
   ],
   [
+    {
+      name: "catalog-get-tokenuri",
+      extractor: {
+        args: [resolve(env.DATA_DIR, "catalog-call-tokenuri-transformation")],
+      },
+      transformer: {},
+    },
+    {
+      name: "mintsongs-get-tokenuri",
+      extractor: {
+        args: [resolve(env.DATA_DIR, "mintsongs-call-tokenuri-transformation")],
+      },
+      transformer: {},
+    },
     {
       name: "soundxyz-get-tokenuri",
       extractor: {
